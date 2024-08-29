a140816fc5cdee5930bbefd94ddcafa7f153b695
Margarita Bliznikova <captainderteufel@gmail.com>
2024-08-29 13:20:40 -0700

    Read the project token from the environment

--- targetZoneKeeper Watch App/targetZoneKeeperApp.swift
+++ targetZoneKeeper Watch App/targetZoneKeeperApp.swift
@@ -7,11 +7,21 @@
 
 import SwiftUI
 
+import Mixpanel
+
 @main
 struct targetZoneKeeper_Watch_AppApp: App {
     @StateObject var heartRateData: HeartRateController = HeartRateController()
     @StateObject var settingsDemonstration: SettingsDemonstrationProvider = SettingsDemonstrationProvider()
-    
+
+    init() {
+        if let projectToken = ProcessInfo.processInfo.environment["MIX_PANEL_PROJECT_TOKEN"] {
+            Mixpanel.initialize(token: projectToken) // TimeZoneKeeper Development
+        } else {
+            print("Can not initialize Mixpanel: no project token for Mixpanel in environment. Events will not be tracked.")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView(heartRateController: heartRateData, settingsDemonstrationProvider: settingsDemonstration)
--- targetZoneKeeper/targetZoneKeeperApp.swift
+++ targetZoneKeeper/targetZoneKeeperApp.swift
@@ -7,9 +7,20 @@
 
 import SwiftUI
 
+import Mixpanel
+
 @main
 struct targetZoneKeeperApp: App {
     @StateObject var settingsLoader: SettingsLoader = SettingsLoader()
+
+    init() {
+        if let projectToken = ProcessInfo.processInfo.environment["MIX_PANEL_PROJECT_TOKEN"] {
+            Mixpanel.initialize(token: projectToken, trackAutomaticEvents: true) // TimeZoneKeeper Development
+        } else {
+            print("Can not initialize Mixpanel: no project token for Mixpanel in environment. Events will not be tracked.")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView(settingsLoader: settingsLoader)
